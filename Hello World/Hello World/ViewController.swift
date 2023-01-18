ac89dd32e88778f55aa2b326a7a27e56d3f61138
jerryzhangshuhao <zhang.shuh@northeastern.edu>
2023-01-17 16:57:59 -0800

    upload HelloWorld

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Jerry Zhang on 1/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lblExample: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("I am inside viewDidLoad() function")
+    }
+    
+    @IBAction func pressMeAction(_ sender: UIButton) {
+        print("Button was pressed")
+        lblExample.text = "Hello World"
+    }
+    
+
+}
